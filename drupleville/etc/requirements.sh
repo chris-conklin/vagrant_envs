7fb5293144711372c2ec22668f81966f0e16da91
cconklin2376 <cconklin2376@gmail.com>
2016-04-21 08:47:09 -0400

    Stabalized requirements and default conf for drupal

--- drupleville/etc/requirements.sh
+++ drupleville/etc/requirements.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # run this after clean env
-cp -r /vagrant/lib/drupalville /var/www/
-apt-get install -y apache2 
-apt-get install -y php5
-apt-get install -y mysql-server
+sudo apt-get install -y apache2 
+sudo apt-get install -y php5
+sudo apt-get install -y php5-gd
+sudo apt-get install -y mysql-server
+sudo apt-get install -y libapache2-mod-auth-mysql php5-mysql phpmyadmin
+
+sudo cp -r /vagrant/lib/drupalville /var/www/
+sudo chown -R www-data.www-data /var/www/drupalville/
 
 sudo cp /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf.orig
 sudo cp /vagrant/etc/000-default.conf /etc/apache2/sites-enabled/000-default.conf
 
+echo "You still need to create a mysql database called drupalville and run the installer"
