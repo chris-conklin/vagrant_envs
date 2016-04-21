96f30e9b8b9fddfc9991661921cbb9c703cf1d2d
cconklin2376 <cconklin2376@gmail.com>
2016-04-20 22:58:34 -0400

    Modified requirements to copy instead of moving apache config files for default

--- drupleville/etc/requirements.sh
+++ drupleville/etc/requirements.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # run this after clean env
-cp /vagrant/drupalville /var/www/
+cp -r /vagrant/lib/drupalville /var/www/
 apt-get install -y apache2 
 apt-get install -y php5
 apt-get install -y mysql-server
+
+sudo cp /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf.orig
+sudo cp /vagrant/etc/000-default.conf /etc/apache2/sites-enabled/000-default.conf
+
