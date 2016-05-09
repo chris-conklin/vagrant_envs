e23357ba2045a7eb51f615a785884654db05b32c
cconklin2376 <cconklin2376@gmail.com>
2016-05-09 10:24:43 -0400

    Add environment for application development

---
+++ ubuntu/ars_services/README.md
@@ -0,0 +1,19 @@
+
+
+
+<h2 style="color: red;">This is a vanilla Drupal & install. Once you vagrant destroy, all of your nodes and changes are GONE!.</h2>
+### Prereqs
+- download drupal - lib/drupal.tar.gz
+- extract drupal - lib/drupal7
+- fire up the vagrant image
+
+### Run provision_drupal.sh
+- install apache2
+- replace the /etc/apache2/sites-enabled/000-default.conf with the one in /vagrant
+- install php5
+- install mysql-server
+- install mysql php playnice packages
+
+### manual work
+- login to mysql and create database drupalville
+- open browser and navigate to the drupalville dir to config instance
---
+++ ubuntu/ars_services/etc/provision_drupal.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# run this after clean env
+sudo apt-get install -y apache2 
+sudo apt-get install -y php5
+sudo apt-get install -y php5-gd
+sudo apt-get install -y libapache2-mod-auth-mysql 
+sudo apt-get install -y php5-mysql 
+sudo apt-get install -y phpmyadmin
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
+sudo apt-get -y install mysql-server
+
+sudo cp -r /vagrant/lib/drupalville /var/www/
+sudo chown -R www-data.www-data /var/www/drupalville/
+
+sudo cp /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf.orig
+sudo cp /vagrant/etc/000-default.conf /etc/apache2/sites-enabled/000-default.conf
+
+sudo service apache2 restart
+
+mysql -uroot -e "CREATE DATABASE drupalville"
+
